e307d56c35d5ba28c9dab49e83d4afedddcdfc7c
QuentinBERITAULT <62297821+QuentinBERITAULT@users.noreply.github.com>
2024-03-19 16:47:14 +0100

    Page Compte Good ! Avec changement Avatar

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-account',
@@ -6,5 +6,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./account.component.css']
 })
 export class AccountComponent {
+    // Caractéristiques du logo
+    logo : any = {
+      imageWidth : 130,
+      imageTitle : "Image",
+      image : "assets/images/logo.svg"
+    };
 
+    img : any = {
+      image : "../../assets/images/user_kids_avatar_user_profile_icon_149314.svg"
+    }
+  isVisible: boolean = false;
+  toggleVisibility(): void {
+    this.isVisible = !this.isVisible;
+  }
+
+  @ViewChild('fileInput') fileInput!: ElementRef;
+
+  openFilePicker(): void {
+    this.fileInput.nativeElement.click();
+  }
+
+  // Méthode pour changer l'image
+  changeImage(event: any): void {
+    const file = event.target.files[0];
+    const reader = new FileReader();
+    reader.onload = (e: any) => {
+      this.img.image = e.target.result;
+    };
+    reader.readAsDataURL(file);
+  }
 }
