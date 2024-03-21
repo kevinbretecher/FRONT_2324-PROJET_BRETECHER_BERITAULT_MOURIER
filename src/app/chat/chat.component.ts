5fbcd63e34450792c1e01f76bf0fc49f637f34a6
QuentinBERITAULT <62297821+QuentinBERITAULT@users.noreply.github.com>
2024-03-21 10:30:48 +0100

    ajout page Chat, avec message seulement à gauche (et peut être que 1 personne)

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./chat.component.css']
 })
 export class ChatComponent {
+  messages: any[] = []; // Stocker les messages
+  newMessage: string = ''; // Nouveau message saisi par l'utilisateur
 
+  // Fonction pour envoyer un message
+  sendMessage() {
+    if (this.newMessage.trim() !== '') {
+      const currentTime = new Date();
+      const fullMessage = {
+        sender: 'Francis', // Mettez le nom de l'utilisateur actuel ici
+        timestamp: currentTime,
+        content: this.newMessage
+      };
+      this.messages.push(fullMessage); // Ajouter le message à la liste des messages
+      this.newMessage = ''; // Réinitialiser la zone de saisie du message
+    }
+  }
 }
